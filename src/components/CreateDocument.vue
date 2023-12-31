7fdb7096ad19453f8b19d7df759d8277c8c51d2e
Laszlo Fazekas <thebojda@gmail.com>
2023-12-31 10:45:02 +0100

    create doc accordion + simplemde

---
+++ src/components/CreateDocument.vue
@@ -0,0 +1,55 @@
+<template>
+  <form>
+    <div class="mb-3">
+      <Accordion>
+        <AccordionTab :header="signer.name" v-for="(signer, idx) in signers" :key="`signer_${idx}`">
+          xxxx
+        </AccordionTab>
+      </Accordion>
+    </div>
+    <div class="mb-3">
+      <label for="document-editor" class="form-label">
+        Document (in Markdown format)
+      </label>
+      <textarea
+        id="document-editor"
+        class="form-control"
+        rows="3"
+        ref="document_editor"
+      ></textarea>
+    </div>
+  </form>
+</template>
+
+<script lang="ts">
+import { Component, Vue, toNative } from "vue-facing-decorator";
+import Accordion from "primevue/accordion";
+import AccordionTab from "primevue/accordiontab";
+
+@Component({
+  components: {
+    Accordion,
+    AccordionTab,
+  },
+})
+class CreateDocument extends Vue {
+  signers = [{ name: "Laszlo Fazekas" }, { name: "Gabor Kiss" }];
+
+  mounted() {
+    let simplemde = new (window as any).SimpleMDE({
+      element: this.$refs.document_editor,
+    });
+  }
+}
+
+export default toNative(CreateDocument);
+</script>
+
+<style>
+.p-accordion .p-accordion-header .p-accordion-header-link {
+    color: #000;
+    background-color: #fff;
+    text-decoration: none;
+    font-weight: 400;
+}
+</style>
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap'
 import 'github-fork-ribbon-css/gh-fork-ribbon.css';
+import 'primevue/resources/themes/lara-light-blue/theme.css'
 import { createApp } from 'vue/dist/vue.esm-bundler.js';
 import PrimeVue from 'primevue/config';
 import SignerProfile from './components/SignerProfile.vue'
 import KYCValidator from './components/KYCValidator.vue'
+import CreateDocument from './components/CreateDocument.vue'
 
 globalThis.__VUE_OPTIONS_API__ = true
 globalThis.__VUE_PROD_DEVTOOLS__ = true;
 
 createApp(SignerProfile).mount('#signer-profile')
 createApp(KYCValidator).use(PrimeVue).mount('#validator')
+createApp(CreateDocument).use(PrimeVue).mount('#create-document')
 
